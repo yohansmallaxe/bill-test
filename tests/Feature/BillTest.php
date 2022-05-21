05ea43f245996fe5ec67be4ef3b7b967cff6fdd6
yohansmallaxe <yohan@smallaxe.tech>
2022-05-21 10:18:25 +0530

    Tests updated

--- tests/Feature/BillTest.php
+++ tests/Feature/BillTest.php
@@ -23,31 +23,31 @@ class BillTest extends TestCase
     {
         $response = $this->actingAs($this->user)->post('api/bill', ['units' => 200]);
         $response->assertStatus(200);
-        $response->assertExactJson(['used units' => 200, 'Bill' => "1,400.00"]);
+        $response->assertExactJson(['used units' => 200, 'Bill' => "920.00"]);
     }
 
     public function test_bill_updates_correctly()
     {
 
-        $this->user->bills()->create(['units' => 200, 'bill' => "1,400.00"]);
+        $this->user->bills()->create(['units' => 200, 'bill' => "920.00"]);
         $response = $this->actingAs($this->user)->put('api/bill/1', ['units' => 80]);
         $response->assertStatus(200);
         $response->assertExactJson(['used units' => 80, 'Bill' => "200.00"]);
     }
     public function test_all_bills_are_showing()
     {
-        $this->user->bills()->create(['units' => 200, 'bill' => "1,400.00"]);
-        $this->user->bills()->create(['units' => 280, 'bill' => "1,880.00"]);
+        $this->user->bills()->create(['units' => 200, 'bill' => "920.00"]);
+        $this->user->bills()->create(['units' => 280, 'bill' => "1,400.00"]);
         $response = $this->actingAs($this->user)->get('api/bills');
         $response->assertStatus(200);
         $response->assertJson(['Bill info' => [
              [
             "Units"=> 200,
-            "Bill"=> "1,400.00",
+            "Bill"=> "920.00",
         ],
         [
             "Units"=> 280,
-            "Bill"=> "1,880.00",
+            "Bill"=> "1,400.00",
         ]]]);
     }
 }
